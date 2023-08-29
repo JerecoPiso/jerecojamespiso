488975b980ceead51187674bab1ea20af0c02362
JerecoPiso <jerecopiso@github.com>
2023-08-29 13:15:51 +0800

    change the services offered section

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -24,7 +24,7 @@ export default function Aboutme() {
                     <Slide triggerOnce={true} direction={'right'}>
                         <p className='text-2xl font-semibold text-gray-300 tracking-wide'>SERVICES OFFERED</p>
                         {/* <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around Web development, but I also extend my services to include Android and Desktop application development.</p> */}
-                        <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around Web development, where I specialize in creating dynamic and responsive websites that meets your unique business needs. Additionally, I extend my services in Android and Desktop application development to have to a wider range of clients in search of innovative applications.</p>
+                        <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around Web development, where I specialize in creating dynamic and responsive websites that meets your unique business needs. Additionally, I extend my services in Android and Desktop application development to have to a wider range of clients in search of innovative digital solutions.</p>
                         <button type='button' className='bg-gray-800 px-5 py-2 text-gray-300 rounded-md mt-3'>DOWNLOAD CV</button>
                     </Slide>
                 </div>
