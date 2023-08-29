bdaebe97a46f8c7aade23c5d252320bf6754b58d
JerecoPiso <jerecopiso@github.com>
2023-08-29 13:14:30 +0800

    change the services offered section

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -10,6 +10,7 @@ export default function Aboutme() {
                     <Slide triggerOnce={true} direction={'left'}>
                         <p className='text-2xl font-semibold text-gray-300 tracking-wide'>GET TO KNOW ME</p>
                         <p className='text-md mt-2 text-gray-300 tracking-wide'>Hey, I'm Jereco James Piso an junior programmer with a passion for creating functional and visually appealing applications. I have a few years of experience in programming, which has allowed me to hone my skills and tackle more complex projects.</p>
+                        <p><FontAwesomeIcon className='mt-3 ' icon={solidIcons.faLocationDot} size="lg" color='#F1F6F9' /><span className='ml-3 text-gray-300 tracking-wide'> Quezon City, Philippines</span> </p>
                         <p><FontAwesomeIcon className='mt-3' icon={solidIcons.faEnvelope} size="lg" color='#F1F6F9' /><span className='ml-3 text-gray-300 tracking-wide'>jamesjerecopiso@gmail.com</span></p>
                         <p><FontAwesomeIcon className='mt-3 ' icon={solidIcons.faPhone} size="lg" color='#F1F6F9' /><span className='ml-3 text-gray-300 tracking-wide'>09518227186</span> </p>
                     </Slide>
@@ -22,7 +23,8 @@ export default function Aboutme() {
                 <div className='col-span-1 xl:text-left lg:text-left md:text-left'>
                     <Slide triggerOnce={true} direction={'right'}>
                         <p className='text-2xl font-semibold text-gray-300 tracking-wide'>SERVICES OFFERED</p>
-                        <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around web development, but I also extend my services to include Android and Desktop application development.</p>
+                        {/* <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around Web development, but I also extend my services to include Android and Desktop application development.</p> */}
+                        <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around Web development, where I specialize in creating dynamic and responsive websites that meets your unique business needs. Additionally, I extend my services in Android and Desktop application development to have to a wider range of clients in search of innovative applications.</p>
                         <button type='button' className='bg-gray-800 px-5 py-2 text-gray-300 rounded-md mt-3'>DOWNLOAD CV</button>
                     </Slide>
                 </div>
