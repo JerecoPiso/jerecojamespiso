877104782dd0855603fc73095d2775cc8a473dac
JerecoPiso <jerecopiso@github.com>
2024-08-23 13:57:17 +0800

    add download cv

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,6 +1,7 @@
 import * as solidIcons from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Slide, Bounce } from "react-awesome-reveal";
+import pdf from '../../src/assets/jerecojamespiso.pdf'
 export default function Aboutme() {
     return (
         <div className='bg-gray-700 xl:p-16 lg:p-16 md:p-14 p-10' id='aboutme'>
@@ -25,7 +26,7 @@ export default function Aboutme() {
                         <p className='text-2xl font-semibold text-gray-300 tracking-wide'>SERVICES OFFERED</p>
                         {/* <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around Web development, but I also extend my services to include Android and Desktop application development.</p> */}
                         <p className='text-md text-gray-300 tracking-wide mt-2'>My primary area of expertise revolves around Web development, where I specialize in creating dynamic and responsive websites that meets your unique business needs. Additionally, I extend my services in Android and Desktop application development to have to a wider range of clients in search of innovative digital solutions.</p>
-                        <button type='button' className='bg-gray-800 px-5 py-2 text-gray-300 rounded-md mt-3'>DOWNLOAD CV</button>
+                        <a href={pdf} type='button' className='bg-gray-800 px-5 py-2 text-gray-300 rounded-md mt-3'>DOWNLOAD CV</a>       
                     </Slide>
                 </div>
             </div>
